51828088420cb42fb36ba098dd2a3439a0ddfa53
juneeda-maker <flora9511@daum.net>
2021-01-07 13:43:10 +0900

    commit

---
+++ codeup/1203.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{   
+    int BMI;
+    scanf("%d", &BMI);
+
+    if(BMI <= 10)
+    {
+        printf("정상");
+    }
+    else if(BMI <= 20)
+    {
+        printf("과체중");
+    }
+    else
+    {
+        printf("비만");
+    }
+    
+    return 0;
+}
