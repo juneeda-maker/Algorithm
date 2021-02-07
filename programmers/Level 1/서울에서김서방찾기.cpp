9686a4d81f0438c7ba477523d1e1a380d98e88fc
juneeda-maker <flora9511@daum.net>
2021-02-07 21:20:46 +0900

    commit

---
+++ programmers/Level 1/서울에서김서방찾기.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+
+    for(int i = 0; i < seoul.size(); i++)
+    {
+        if(seoul[i] == "Kim")
+        {
+            answer +=  "김서방은 ";
+            answer +=  to_string(i);
+            answer +=  "에 있다";
+        }    
+    }
+    
+
+    return answer;
+}
+
+int main(void){
+    vector<string> seoul = {"Jane", "Kim"};
+    cout << solution(seoul);
+}
