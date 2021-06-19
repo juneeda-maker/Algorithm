1ef30468ac11270ce00f622a0e75b391b7f82b61
juneeda-maker <flora9511@daum.net>
2021-06-20 00:02:45 +0900

    commit

---
+++ programmers/Level2/hindex.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    if(!citations[0])
+    {
+        return 0;
+    }
+
+    for(int i = 0; i < citations.size(); i++)
+    {
+        if(citations[i] > i)
+            answer++;
+        else
+            break;
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<int> citations = {4,3,3,3,3};
+    cout << solution(citations);
+}
