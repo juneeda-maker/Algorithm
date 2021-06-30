d6a6cb1e4df0a21e8844ae68d7ada8e9b6e2d8ef
juneeda-maker <flora9511@daum.net>
2021-06-30 21:56:53 +0900

    commit

---
+++ programmers/Level2/2개이하로다른비트.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        if(numbers[i] % 2 == 0)
+        {
+            answer.push_back(numbers[i] + 1);
+        }
+        else
+        {
+            long long bit = 1;
+            while(true)
+            {
+                if((numbers[i] & bit) == 0)
+                {
+                    break;
+                }
+                bit = bit * 2;
+            }
+            bit = bit / 2;
+            answer.push_back(numbers[i] + bit);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    vector<long long> numbers = {2,7};
+    solution(numbers);
+}
