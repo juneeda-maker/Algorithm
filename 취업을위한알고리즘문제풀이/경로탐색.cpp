b3674266a0698d769be3d8a00cf1a85b67ec29b4
juneeda-maker <flora9511@daum.net>
2021-07-27 21:45:15 +0900

    commit

---
+++ 취업을위한알고리즘문제풀이/경로탐색.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+using namespace std;
+
+int map[30][30], ch[30], cnt = 0, n;
+
+void DFS(int v){
+    int i;
+    if(v == n)
+    {
+        cnt++;
+    }
+    else
+    {
+        for(i = 1; i <= n; i++)
+        {
+            if(map[v][i] == 1 && ch[i] == 0)
+            {
+                ch[i] = 1;
+                DFS(i);
+                ch[i] = 0;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int m, i, j, a, b;
+
+    scanf("%d %d", &n, &m);
+
+    for(i = 1; i <= m; i++)
+    {
+        scanf("%d %d", &a, &b);
+        map[a][b] = 1;
+    }
+
+    ch[1] = 1;
+    DFS(1);
+
+    printf("%d", &cnt);
+
+    return 0;
+}
