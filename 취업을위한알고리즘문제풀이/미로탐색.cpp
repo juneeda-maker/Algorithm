d96d2e1794e19ab6e0d2a12f145c3df16950b33f
juneeda-maker <flora9511@daum.net>
2021-07-30 23:18:35 +0900

    commit

--- 취업을위한알고리즘문제풀이/미로탐색.cpp
+++ 취업을위한알고리즘문제풀이/미로탐색.cpp
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
-int map[10][10], ch[10], cnt = 0;
+int map[10][10], ch[10][10], cnt = 0;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-void DFS(int v)
+void DFS(int x, int y)
 {
-    int i;
-    if(v == 7)
+    int i , xx, yy;
+    if(x == 7 && y == 7)
     {
         cnt++;
     }
     else
     {
-        for(i = 1; i <= 7; i++)
+        for(i = 0; i <= 4; i++)
         {
-            if(map[v][i] == 1 && ch)
+            xx = x + dx[i];
+            yy = y + dy[i];
+            if(xx < 1 || xx > 7 || yy < 1 || yy > 7) continue;
+            if(map[xx][yy] == 0 && ch[xx][yy] == 0)
             {
-                DFS(i);
+                ch[xx][yy] = 1;
+                DFS(xx, yy);
+                ch[xx][yy] = 0;
             }
         }
     }
@@ -23,16 +30,17 @@ void DFS(int v)
 
 int main(){
 
-    int i, j, a;
+    int i, j;
 
     for(i = 1; i<= 7; i++)
     {
         for(j = 1; j <= 7; j++)
         {
-            scanf("%d", a);
-            map[i][j] = a;
+            scanf("%d", &map[i][j]);
         }
     }
-
+    ch[1][1] = 1;
+    DFS(1, 1);
+    printf("%d\n", cnt);
     return 0;
 }
